b6944d9cb647308adcefd079f3383017970d8dee
jessenatter <125529115+jessenatter@users.noreply.github.com>
2023-03-04 14:00:01 -0500

    loop and tree spawner works

---
+++ lipcritic1.1/Assets/scripts/TreeSpawner.cs
@@ -0,0 +1,46 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TreeSpawner : MonoBehaviour
+{
+
+    public GameObject tree;
+    private Vector3 Location;
+    private Quaternion Rotation;
+    public GameObject MapStart;
+    public GameObject MapEnd;
+
+    private float Mstart;
+    private float Mend;
+    private float TreeNumb;
+
+    private float xpos;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Mstart = MapStart.transform.position.x;
+        Mend = MapEnd.transform.position.x;
+
+        //number of trees will be deternimed by the distance between the map start and end, devided by 4, because distance bewteen each tree is between 3 and 4 
+
+        TreeNumb = (Mathf.Abs(Mend) + Mathf.Abs(Mstart)) / 4;
+
+        //i being x cord
+        for (float i = 1; i < TreeNumb; i++)
+        {
+            xpos = Mstart + i * Random.Range(2f, 3f);
+            //instantiate tree with z rotation between 22 and -22, x pos that is between 3 and 4 units away, and y pos between 2 and 3.5
+           Location = new Vector3(xpos, 2f, Random.Range(5f,10f));
+           Rotation = Quaternion.Euler(0, 0, Random.Range(-22, 22));
+           Instantiate(tree, Location, Rotation);
+            
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
